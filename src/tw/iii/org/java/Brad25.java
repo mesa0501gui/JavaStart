60524fe79ef970c7fdf6cbe8d370dc965230ccc0
mesa0501gui <mesa0501gui@gmail.com>
2016-08-31 16:41:40 +0800

    vv

---
+++ src/tw/iii/org/java/Brad25.java
@@ -0,0 +1,16 @@
+package tw.iii.org.java;
+
+public class Brad25 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        
+        if(TWid.checkRight("A123456782")){
+        	System.out.println("OK");
+        }else 
+        	System.out.println("xx");
+        
+        
+	}
+
+}
---
+++ src/tw/iii/org/java/BradTextPro.java
@@ -0,0 +1,11 @@
+package tw.iii.org.java;
+
+public class BradTextPro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
